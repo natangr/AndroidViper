77e118b7f1d0aa10de0772f1f09ce0ffb5c9d60f
Natan Grando <natan@lockitron.com>
2017-04-04 18:03:15 -0300

    Add api comment on interactor

--- app/src/main/java/io/ckl/androidviper/modules/login/LoginInteractor.kt
+++ app/src/main/java/io/ckl/androidviper/modules/login/LoginInteractor.kt
@@ -3,6 +3,7 @@ package io.ckl.androidviper.modules.login
 class LoginInteractor(var output: LoginContract.InteractorOutput?): LoginContract.Interactor {
 
     override fun performLogin(username: String, password: String) {
+        // Here is where you should handle the API event. You could use an ApiDataManager helper to create the request. This way you keep all the API code centralized.
         if (username == "natan" && password == "good") {
             output?.onLoginSucceeded()
         } else {
